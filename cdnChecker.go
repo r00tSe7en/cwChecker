9884f41e4c9ee60f03794dc68ee367aa43f95e96
Se7en <455732253@qq.com>
2024-11-23 20:11:53 +0800

    Add files via upload

---
+++ cdnChecker.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Workiva/go-datastructures/queue"
+	"github.com/miekg/dns"
+	"github.com/projectdiscovery/cdncheck"
+	"github.com/projectdiscovery/dnsx/libs/dnsx"
+	"github.com/projectdiscovery/retryabledns"
+	"io/ioutil"
+	"math"
+	"math/rand"
+	"net"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+)
+
+var validResolversList []string //可用dns服务器列表
+var noCdnDomains []string       //未使用cdn的域名列表
+var useCdnDomains []string      //使用cdn的域名列表
+var noCdnIps []string           //未使用cdn的ip列表
+var domainsInfo []string        //所有域名+对应ip信息记录在此列表中
+var wg sync.WaitGroup
+var matchedCDN bool
+var matchedWAF bool
+
+// 将文件内容转为字符列表，主要用于域名列表、cdn cname列表与dns服务器
+func FileContentToList(filePath string) []string {
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("fail to open file: " + filePath)
+		os.Exit(2)
+	}
+
+	fileContentStr := strings.ReplaceAll(string(fileContent), "\r\n", "\n")
+	contentList := strings.Split(fileContentStr, "\n")
+	var newList []string
+	for _, element := range contentList {
+		if element != "" {
+			newList = append(newList, element)
+		}
+	}
+	return newList
+}
+
+// 判断字符串在数组元素中是否包含
+func In(target string, str_array []string) bool {
+	sort.Strings(str_array)
+	index := sort.SearchStrings(str_array, target)
+	if index < len(str_array) && str_array[index] == target {
+		return true
+	}
+	return false
+}
+
+// 通过解析特定域名获取ip地址来找出提供列表中可用dns服务器
+func FilterValidResolver(resolver string) {
+	defer wg.Done()
+	retries := 1
+	tempResolverList := []string{resolver}
+	dnsClient, _ := retryabledns.New(tempResolverList, retries)
+	dnsResponses, _ := dnsClient.Query("public1.114dns.com", dns.TypeA)
+	if In("114.114.114.114", dnsResponses.A) {
+		validResolversList = append(validResolversList, resolver)
+	}
+}
+
+// 判断域名的cname是否包含cdn cname，如果包含则判定使用了cdn
+func InCdnCnameList(domainCnameList []string, cdnCnameList []string) bool {
+	inCdnCname := false
+	for _, domainCname := range domainCnameList {
+		for _, cdnCname := range cdnCnameList {
+			if strings.Contains(domainCname, cdnCname) {
+				inCdnCname = true
+				return inCdnCname
+			}
+		}
+	}
+	return inCdnCname
+}
+
+// 获取域名在特定dns上的解析ip，并且以.分割ip，取ip的前三部分，即解析ip为1.1.1.1,最终输出为[1.1.1],便于判断多个ip是否在相同网段
+func ResolvDomainIpPart(domain string, resolver string) ([]string, error) {
+	var domainIpsPart []string
+	retries := 1
+	resolverList := []string{resolver}
+	dnsClient, err := retryabledns.New(resolverList, retries)
+	if err != nil {
+		return domainIpsPart, err
+	}
+	dnsResponses, err := dnsClient.Query(domain, dns.TypeA)
+	if err != nil {
+		return domainIpsPart, err
+	} else if In(resolver, dnsResponses.A) { //如果dns的ip出现在查询结果中，判为误报，忽略结果
+		return domainIpsPart, nil
+	}
+	ipsList := dnsResponses.A //[1.1.1.1, 2.2.2.2, 3.3.3.3]
+	if len(ipsList) > 0 {
+		for _, ip := range ipsList {
+			ipParts := strings.Split(ip, ".")
+			ipSplit := ipParts[0] + "." + ipParts[1] + "." + ipParts[2]
+			domainIpsPart = UniqueStrList(append(domainIpsPart, ipSplit))
+		}
+	}
+	return domainIpsPart, nil
+}
+
+// 字符型列表去重去空
+func UniqueStrList(strList []string) []string {
+	uniqList := make([]string, 0)
+	tempMap := make(map[string]bool, len(strList))
+	for _, v := range strList {
+		if tempMap[v] == false && len(v) > 0 {
+			tempMap[v] = true
+			uniqList = append(uniqList, v)
+		}
+	}
+	return uniqList
+}
+
+// 生成count个[start,end)结束的不重复的随机数
+func GenerateRandomNumber(start int, end int, count int) []int {
+	//范围检查
+	if end < start || (end-start) < count {
+		return nil
+	}
+
+	//存放结果的slice
+	nums := make([]int, 0)
+	//随机数生成器，加入时间戳保证每次生成的随机数不一样
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for len(nums) < count {
+		//生成随机数
+		num := r.Intn((end - start)) + start
+
+		//查重
+		exist := false
+		for _, v := range nums {
+			if v == num {
+				exist = true
+				break
+			}
+		}
+
+		if !exist {
+			nums = append(nums, num)
+		}
+	}
+
+	return nums
+}
+
+// 判断域名是否使用cdn
+func CdnCheck(domain string, cdnCnameList []string, resolversList []string, dnsxClient *dnsx.DNSX) {
+	defer wg.Done()
+
+	isCDNorWAF, _, _ := dnsxClient.CdnCheck(domain) //通过dnsx自带方法识别cdn，主要为根据ip范围判断国外主流cdn厂商
+	dnsxResult, _ := dnsxClient.QueryOne(domain)
+	domainCnameList := dnsxResult.CNAME
+	tempDomainIpList := dnsxResult.A
+	var domainIpList []string
+	flag := 0
+	if len(tempDomainIpList) > 0 {
+		for _, tempIp := range tempDomainIpList {
+			if !In(tempIp, resolversList) {
+				domainIpList = append(domainIpList, tempIp)
+			}
+		}
+	}
+	// Check if the domain uses a CDN directly through dnsx
+	if isCDNorWAF {
+		println(domain + " dnsx域名判断cdn")
+		useCdnDomains = append(useCdnDomains, domain)
+	}
+	// Iterate through the IPs and perform further checks
+	for _, domainIp := range domainIpList {
+		flag = flag + 1
+		// Add the domain and IP pair to the info list
+		domainsInfo = UniqueStrList(append(domainsInfo, domain+" "+domainIp))
+		// Call CheckCDNAndWAF to check if the IP is behind a CDN or WAF
+		matchedCDN, matchedWAF, err := CheckCDNAndWAF(domainIp)
+		if err != nil {
+			fmt.Println(err) // Handle the error appropriately
+			continue
+		}
+
+		if matchedCDN || matchedWAF {
+			// Add domain to CDN list
+			useCdnDomains = append(useCdnDomains, domain)
+			isCDNorWAF = true
+		}
+		//One domain name resolves multiple IPs
+		if flag > 4 {
+			isCDNorWAF = true
+		}
+	}
+	if isCDNorWAF {
+		println(domain + " IP Check cdn/waf YES")
+	} else {
+		// If the domain doesn't have CDN-related CNAME or A record, mark it as no CDN
+		if len(domainCnameList) == 0 && len(domainIpList) > 0 {
+			noCdnDomains = append(noCdnDomains, domain)
+			println(domain + " 0cname+Nip cdn NO")
+			noCdnIps = UniqueStrList(append(noCdnIps, domainIpList...))
+		} else if len(domainCnameList) > 0 && len(domainIpList) > 0 {
+			// Check if CNAME matches CDN list
+			if InCdnCnameList(domainCnameList, cdnCnameList) {
+				println(domain + " Ncname+Nip cdn YES")
+				useCdnDomains = append(useCdnDomains, domain)
+			} else {
+				var domainIpPartList []string
+				randNums := GenerateRandomNumber(0, len(resolversList), 30)
+				for _, num := range randNums {
+					resolver := resolversList[num]
+					domainIpsWithResolver, err := ResolvDomainIpPart(domain, resolver)
+					if err != nil {
+						continue
+					}
+					domainIpPartList = UniqueStrList(append(domainIpPartList, domainIpsWithResolver...))
+					if len(domainIpPartList) > 3 { // If 4 different subnets are found
+						break
+					}
+				}
+				if len(domainIpPartList) > 3 {
+					println(domain + " domainIpPartList>3 cdn YES")
+					useCdnDomains = append(useCdnDomains, domain)
+				} else {
+					println(domain + " domainIpPartList<3 cdn NO")
+					noCdnDomains = append(noCdnDomains, domain)
+					noCdnIps = UniqueStrList(append(noCdnIps, domainIpList...))
+				}
+			}
+		}
+	}
+
+}
+
+// 检查IP是否属于CDN或WAF
+func CheckCDNAndWAF(ip string) (bool, bool, error) {
+	// 将字符串形式的IP转换为 net.IP 类型
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		return false, false, fmt.Errorf("invalid IP address: %s", ip)
+	}
+
+	client := cdncheck.New() // Initialize the CDN client
+	// Check if the IP is behind a CDN
+	matchedCDN, _, err := client.CheckCDN(parsedIP)
+	if err != nil {
+		return false, false, fmt.Errorf("CDN check error: %s", err.Error())
+	}
+
+	// Check if the IP is behind a WAF
+	matchedWAF, _, err := client.CheckWAF(parsedIP)
+	if err != nil {
+		return false, false, fmt.Errorf("WAF check error: %s", err.Error())
+	}
+	return matchedCDN, matchedWAF, nil
+}
+
+func main() {
+	nowTime := time.Now().Format("200601021504")
+	df := flag.String("df", "", "domain list file")
+	cf := flag.String("cf", "cdn_cname", "cdn cname file")
+	r := flag.String("r", "", "dns resolvers file")
+	o := flag.String("o", "no_cdn_domains"+nowTime+".txt", "output domains that are not using cdn to file")
+	oi := flag.String("oi", "no_cdn_ips"+nowTime+".txt", "output ips that are not using cdn to file")
+	oc := flag.String("oc", "use_cdn_domains"+nowTime+".txt", "output domains that are using cdn to file")
+	od := flag.String("od", "domain_info"+nowTime+".txt", "output domain info(domain:ip) to file")
+	flag.Parse()
+
+	noCdnDomainsFileName := *o
+	noCdnIpsFileName := *oi
+	useCdnDomainsFileName := *oc
+	domainsInfoFileName := *od
+
+	//get domains list
+	domainsListFile := *df
+	var domainsList []string
+	domainsList = UniqueStrList(FileContentToList(domainsListFile))
+
+	//get cdn cname list
+	cdnCnameFile := *cf
+	var cdnCnameList []string
+	cdnCnameList = UniqueStrList(FileContentToList(cdnCnameFile))
+
+	//get resolvers list
+	resolversFile := *r
+	tempResolversList := UniqueStrList(FileContentToList(resolversFile))
+
+	queResolvers := queue.New(5) //dns服务器队列
+	for _, resolver := range tempResolversList {
+		queResolvers.Put(resolver)
+	}
+	for queResolvers.Len() > 0 {
+		wg.Add(1)
+		queResolverList, _ := queResolvers.Get(1)
+		queResolver := queResolverList[0].(string)
+		go FilterValidResolver(queResolver)
+	}
+	wg.Wait()
+	if len(validResolversList) < 20 {
+		fmt.Println("The number of valid resolvers is less than 20, May affect the accuracy of the results")
+	}
+
+	var DefaultOptions = dnsx.Options{
+		BaseResolvers:     validResolversList,
+		MaxRetries:        5,
+		QuestionTypes:     []uint16{dns.TypeA},
+		TraceMaxRecursion: math.MaxUint16,
+		Hostsfile:         true,
+	}
+	//init dnsx client
+	dnsxClient, _ := dnsx.New(DefaultOptions)
+
+	que := queue.New(5) //域名队列
+
+	for _, domain := range domainsList {
+		que.Put(domain)
+	}
+
+	for que.Len() > 0 {
+		wg.Add(1)
+		queDomainList, _ := que.Get(1)
+		queDomain := queDomainList[0].(string)
+
+		go CdnCheck(queDomain, cdnCnameList, validResolversList, dnsxClient)
+	}
+	wg.Wait()
+
+	//未使用cdn域名写入文件
+	if len(noCdnDomains) > 0 {
+		noCdnDomains = UniqueStrList(noCdnDomains)
+		noCdnDomainsFile, _ := os.OpenFile(noCdnDomainsFileName, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666)
+		defer noCdnDomainsFile.Close()
+		for _, noCdnDomain := range noCdnDomains {
+			noCdnDomainsFile.WriteString(noCdnDomain + "\n")
+		}
+	}
+
+	//未使用cdn的ip写入文件
+	if len(noCdnIps) > 0 {
+		noCdnIps = UniqueStrList(noCdnIps)
+		noCdnIpsFile, _ := os.OpenFile(noCdnIpsFileName, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666)
+		defer noCdnIpsFile.Close()
+		for _, noCdnIp := range noCdnIps {
+			//fmt.Println(noCdnIp)
+			noCdnIpsFile.WriteString(noCdnIp + "\n")
+		}
+	}
+
+	//使用cdn域名写入文件
+	if len(useCdnDomains) > 0 {
+		useCdnDomains = UniqueStrList(useCdnDomains)
+		useCdnDomainsFile, _ := os.OpenFile(useCdnDomainsFileName, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666)
+		defer useCdnDomainsFile.Close()
+		for _, useCdnDomain := range useCdnDomains {
+			useCdnDomainsFile.WriteString(useCdnDomain + "\n")
+		}
+	}
+
+	//域名+对应ip信息写入文件
+	if len(domainsInfo) > 0 {
+		domainsInfo = UniqueStrList(domainsInfo)
+		domainsInfoFile, _ := os.OpenFile(domainsInfoFileName, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666)
+		defer domainsInfoFile.Close()
+		for _, domainInfo := range domainsInfo {
+			domainsInfoFile.WriteString(domainInfo + "\n")
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,38 @@
+module cdnChecker
+
+go 1.22.1
+
+require (
+	github.com/Workiva/go-datastructures v1.1.5
+	github.com/miekg/dns v1.1.62
+	github.com/projectdiscovery/cdncheck v1.1.0
+	github.com/projectdiscovery/dnsx v1.2.1
+	github.com/projectdiscovery/retryabledns v1.0.85
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/gaissmai/bart v0.9.5 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/projectdiscovery/blackrock v0.0.1 // indirect
+	github.com/projectdiscovery/utils v0.2.18 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/weppos/publicsuffix-go v0.30.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20230420155640-133eef4313cb // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
